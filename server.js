a4223eb44f5349704ad947c6c0e55134cff6807c
prabhanshu dubey <you@example.com>
2019-02-06 11:19:15 +0530

    [Master]: Database setup

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB config keys
+const db = require('./config/keys').mongoURI;
+
+mongoose
+    .connect(db)
+    .then(()=> console.log('Mongo DB connected'))
+    .catch((error)=> console.log(error));
 
 app.get('/', (req, res) => res.send('Hello!'));
 
