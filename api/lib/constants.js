021704c73e43330bf4d41e4c9b8c884d5e710e58
prabhanshu dubey <you@example.com>
2019-02-11 17:31:40 +0530

    [DevMeet-1]: More validations

--- api/lib/constants.js
+++ api/lib/constants.js
@@ -1,13 +1,19 @@
 exports.ResponseErrors = Object.freeze({
-   EMAIL_ALREADY_EXISTS: 'Email already Exists',
-   USER_NOT_FOUND: 'User not found',
-   INVALID_USERNAME_PASSWORD: 'Invalid username and password'
+    EMAIL_ALREADY_EXISTS: 'Email already Exists',
+    USER_NOT_FOUND: 'User not found',
+    INVALID_USERNAME_PASSWORD: 'Invalid username and password'
 });
 
 exports.ResponseSuccess = Object.freeze({
-   SUCCESSFULLY_SIGNED_IN: 'User successfully signed in'
+    SUCCESSFULLY_SIGNED_IN: 'User successfully signed in'
 });
 
 exports.ValidationErrors = Object.freeze({
-   NAME_VALIDATION: "Name must be between 2 and 10"
+    NAME_VALIDATION: "Name must be between 2 and 10",
+    NAME_EMPTY: 'Name is required',
+    PASSWORD_EMPTY: 'Password is required',
+    CONFIRMATION_PASSWORD_EMPTY: 'Confirm Password is required',
+    EMAIL_VALIDATION: 'Email is invalid',
+    PASSWORD_VALIDATION: "Name must be between 6 and 30",
+    CONFIRMATION_PASSWORD_VALIDATION: "Passwords do not match",
 });
--- api/validations/register_validator.js
+++ api/validations/register_validator.js
@@ -4,6 +4,13 @@ import isEmpty from './is_empty';
 
 const NAME_MIN_LEN = 2;
 const NAME_MAX_LEN = 10;
+const PASSWORD_MIN_LEN = 6;
+const PASSWORD_MAX_LEN = 30;
+
+data.name = !isEmpty(data.name) || '';
+data.email = !isEmpty(data.email) || '';
+data.password = !isEmpty(data.password) || '';
+data.confirmPassword = !isEmpty(data.confirmPassword) || '';
 
 /**
  * Method to validate register input
@@ -17,6 +24,30 @@ module.exports = function validateRegisterInput(data) {
 
     if (!Validator.isLength(data.name, {min: NAME_MIN_LEN, max: NAME_MAX_LEN})) {
         errors.name = ResponseMessage.ValidationErrors.NAME_VALIDATION
+    }
+
+    if (Validator.isEmail(data.email)) {
+        errors.email = ResponseMessage.ValidationErrors.NAME_EMPTY;
+    }
+
+    if (Validator.isEmpty(data.password)) {
+        errors.password = ResponseMessage.ValidationErrors.PASSWORD_EMPTY;
+    }
+
+    if (Validator.isEmpty(data.confirmPassword)) {
+        errors.confirmPassword = ResponseMessage.ValidationErrors.CONFIRMATION_PASSWORD_EMPTY;
+    }
+
+    if (Validator.isLength(data.password, {min: PASSWORD_MIN_LEN, max: PASSWORD_MAX_LEN})) {
+        errors.password = ResponseMessage.ValidationErrors.PASSWORD_VALIDATION;
+    }
+
+    if (Validator.isLength(data.password)) {
+        errors.password = ResponseMessage.ValidationErrors.PASSWORD_VALIDATION;
+    }
+
+    if (data.password !== data.confirmPassword) {
+        errors.confirmPassword = ResponseMessage.ValidationErrors.CONFIRMATION_PASSWORD_VALIDATION;
     }
 
     return {
